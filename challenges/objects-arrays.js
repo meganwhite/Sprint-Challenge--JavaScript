43fbcd565756c154e7ae6b66db1240c842f8fb3c
meganwhite <mfw247@gmail.com>
2019-06-14 11:49:24 -0400

    completed array methods exercises

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,8 +74,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+let universities = graduates.map(graduate => {
+  return graduate.university;
+});
+universities = universities.sort();
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -83,12 +86,16 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(graduate => {
+  return `${graduate.first_name} ${graduate.email}`;
+});
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter(graduate => {
+  return graduate.university.includes('Uni');
+});
 console.log(uni);
 
 
